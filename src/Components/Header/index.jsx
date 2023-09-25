22bcbe0e53c68fca75984f7f4680016759b063f8
chris <chris@DESKTOP-AMOTF7V>
2023-09-25 16:41:15 -0400

    starting the project

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img className="header__img" src="/img/Header.jpg"></img>
+            <h1 className="header__title">Plate Palette</h1>
+        </header>
+    )
+};
+
+
+export default Header;
